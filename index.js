1922c384a0ba603a6d78ac67bc847653fc76ec76
jlrpuma <jlrp39@gmail.com>
2017-02-17 11:26:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.post('/', cors() , function(req, res) {
   //res.send('hello world');
 });
 
-app.post('/messageToOne', cors() , function(req, res) {
+app.post('/messageToOne/', cors() , function(req, res) {
   console.log(req.body.message);
   console.log(req.body);
   //sendNotification(req.body.message);
